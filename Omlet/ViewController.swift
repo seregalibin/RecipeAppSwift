8d3c0626dbbbc3cdbce767823c62d0edde5da585
Sergey Libin <serge.libin@gmail.com>
2017-07-25 14:54:53 +0300

    Initial Commit

---
+++ Omlet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Omlet
+//
+//  Created by Sergey Libin on 25.07.17.
+//  Copyright © 2017 Sergey Libin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
